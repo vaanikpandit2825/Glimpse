f87c59338729ef9459b45d0d8e519c2337a5f78c
vaanikpandit2825 <vaanikpandit@gmail.com>
2026-07-21 16:03:37 +0530

    Implement session persistence

--- app/src/main/java/com/example/glimpse/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/glimpse/navigation/AppNavigation.kt
@@ -7,10 +7,19 @@ import androidx.navigation.compose.rememberNavController
 import com.example.glimpse.auth.SignupScreen
 import com.example.glimpse.auth.LoginScreen
 import com.example.glimpse.ui.HomeScreen
+import com.google.firebase.auth.FirebaseAuth
 
 @Composable
 fun AppNavigation(){
     val navController= rememberNavController()
+    val user = FirebaseAuth.getInstance().currentUser
+
+    val startDestination = if(user!=null){
+        "home"
+    }
+    else{
+        "signup"
+    }
     NavHost(
         navController = navController,
         startDestination = "signup"
