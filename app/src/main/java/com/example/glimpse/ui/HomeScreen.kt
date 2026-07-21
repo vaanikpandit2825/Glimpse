8cc86c56f8d673943359cd0d4d6758bb3cae962b
vaanikpandit2825 <vaanikpandit@gmail.com>
2026-07-21 16:25:28 +0530

    Implement logout and session termination

--- app/src/main/java/com/example/glimpse/ui/HomeScreen.kt
+++ app/src/main/java/com/example/glimpse/ui/HomeScreen.kt
@@ -7,15 +7,40 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.ui.unit.dp
+import com.google.firebase.auth.FirebaseAuth
+
 
 @Composable
 fun HomeScreen(
     navController: NavController
 ) {
-    Box(
+    Column(
         modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Text("Welcome to Glimpse")
+        Text("Welcome To Glimpse")
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick={
+                FirebaseAuth.getInstance().signOut()
+                navController.navigate("signup"){
+                    popUpTo("home"){
+                        inclusive=true
+                    }
+                }
+            }
+        ){
+            Text("Logout")
+        }
     }
 }
