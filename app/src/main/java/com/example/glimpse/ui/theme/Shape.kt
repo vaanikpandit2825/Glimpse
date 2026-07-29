5dc30efb8f36e3dc0e6f5eb428c143a2972a4b34
vaanikpandit2825 <vaanikpandit@gmail.com>
2026-07-29 19:11:51 +0530

    add Glimpse top controls component

---
+++ app/src/main/java/com/example/glimpse/ui/theme/Shape.kt
@@ -0,0 +1,12 @@
+package com.example.glimpse.ui.theme
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes=Shapes(
+    extraSmall = RoundedCornerShape(8.dp),
+    small=RoundedCornerShape(12.dp),
+    medium= RoundedCornerShape(20.dp),
+    large=RoundedCornerShape(28.dp),
+    extraLarge=RoundedCornerShape(32.dp)
+)
