4b066c1dc30d482050942a07a76e55514ba24116
vaanikpandit2825 <vaanikpandit@gmail.com>
2026-07-29 19:10:25 +0530

    add Glimpse top controls component

--- app/src/main/java/com/example/glimpse/ui/theme/Color.kt
+++ app/src/main/java/com/example/glimpse/ui/theme/Color.kt
@@ -2,10 +2,16 @@ package com.example.glimpse.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Background = Color(0xFFFAFAF8)
+val Surface = Color(0xFFFFFFFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val TextPrimary = Color(0xFF2B2A28)
+val TextSecondary = Color(0xFF6B6A67)
+
+val Border = Color(0xFFE5E7EB)
+
+val OceanBlue = Color(0xFF00689B)
+val OceanBlueDark = Color(0xFF003773)
+
+val Success = Color(0xFF4CAF50)
+val Error = Color(0xFFD64545)
