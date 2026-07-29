4b173b3715eb42ae21d7fcaaa86f0cf604b70532
vaanikpandit2825 <vaanikpandit@gmail.com>
2026-07-29 19:11:11 +0530

    add Glimpse top controls component

---
+++ app/src/main/java/com/example/glimpse/ui/screens/HomeScreen.kt
@@ -0,0 +1,33 @@
+package com.example.glimpse.ui.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import org.maplibre.compose.map.MaplibreMap
+import org.maplibre.compose.style.BaseStyle
+import com.example.glimpse.BuildConfig
+
+
+@Composable
+fun HomeScreen(
+    navController:NavController
+){
+    Scaffold {  innerPadding ->
+        Box(
+            modifier=Modifier.fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            MaplibreMap(
+                modifier = Modifier.fillMaxSize(),
+                baseStyle = BaseStyle.Uri(
+                    "https://api.maptiler.com/maps/streets-v2/style.json?key=${BuildConfig.MAPTILER_API_KEY}"
+                )
+            )
+        }
+
+    }
+}
