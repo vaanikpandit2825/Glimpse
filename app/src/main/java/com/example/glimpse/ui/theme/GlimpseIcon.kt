a38086594c047e6ca65b844ce9776a6e9ab51d3e
vaanikpandit2825 <vaanikpandit@gmail.com>
2026-08-03 23:05:20 +0530

    establish reusable UI foundation for bottom sheet features

---
+++ app/src/main/java/com/example/glimpse/ui/theme/GlimpseIcon.kt
@@ -0,0 +1,14 @@
+package com.example.glimpse.ui.theme
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.DirectionsCar
+import androidx.compose.material.icons.rounded.LocationOn
+import androidx.compose.material.icons.rounded.Place
+import androidx.compose.material.icons.rounded.Warning
+
+object GlimpseIcon{
+    val location=Icons.Rounded.LocationOn
+    val driving=Icons.Rounded.DirectionsCar
+    val Sos=Icons.Rounded.Warning
+    val CheckIn=Icons.Rounded.Place
+}
