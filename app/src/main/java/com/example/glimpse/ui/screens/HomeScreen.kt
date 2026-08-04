e2e94d21dc9980889187c8f5622a3a1e26e58daa
vaanikpandit2825 <vaanikpandit@gmail.com>
2026-08-05 00:24:10 +0530

    implement check-in bottom sheet UI with reusable components

--- app/src/main/java/com/example/glimpse/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/glimpse/ui/screens/HomeScreen.kt
@@ -3,8 +3,10 @@ package com.example.glimpse.ui.screens
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -12,23 +14,29 @@ import androidx.navigation.NavController
 import com.example.glimpse.BuildConfig
 import com.example.glimpse.R
 import com.example.glimpse.model.Friend
+import com.example.glimpse.ui.components.BottomSheets.CheckInBottomSheet
 import com.example.glimpse.ui.components.FriendsBottomSheet
 import com.example.glimpse.ui.components.MyLocationButton
 import com.example.glimpse.ui.components.TopControls
 import org.maplibre.compose.map.MaplibreMap
 import org.maplibre.compose.style.BaseStyle
 
-
 @Composable
 fun HomeScreen(
     navController: NavController
 ) {
+
+    var showCheckInSheet by remember {
+        mutableStateOf(false)
+    }
+
     Scaffold { innerPadding ->
+
         val friends = listOf(
             Friend(
                 id = "1",
                 name = "Aryan",
-                avatar = R.drawable.ic_launcher_foreground, // Temporary
+                avatar = R.drawable.ic_launcher_foreground,
                 location = "SRM Library",
                 lastSeen = "Just now"
             ),
@@ -47,6 +55,7 @@ fun HomeScreen(
                 lastSeen = "5 min ago"
             )
         )
+
         Box(
             modifier = Modifier
                 .fillMaxSize()
@@ -76,6 +85,24 @@ fun HomeScreen(
                 friends = friends,
                 modifier = Modifier.align(Alignment.BottomCenter)
             )
+
+            FloatingActionButton(
+                onClick = {
+                    showCheckInSheet = true
+                },
+                modifier = Modifier
+                    .align(Alignment.Center)
+            ) {
+                Text("CI")
+            }
+
+            if (showCheckInSheet) {
+                CheckInBottomSheet(
+                    onDismiss = {
+                        showCheckInSheet = false
+                    }
+                )
+            }
         }
     }
 }
