85c7dbcaa332bd551360f212376349d2e77be960
vaanikpandit2825 <vaanikpandit@gmail.com>
2026-08-05 00:23:39 +0530

    implement check-in bottom sheet UI with reusable components

--- app/src/main/java/com/example/glimpse/ui/components/BottomSheetHeader.kt
+++ app/src/main/java/com/example/glimpse/ui/components/BottomSheetHeader.kt
@@ -1,2 +1,45 @@
 package com.example.glimpse.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun BottomSheetHeader(
+    title:String
+){
+    Column(
+        modifier=Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ){
+        Box(
+            modifier= Modifier
+                .width(40.dp)
+                .height(5.dp)
+                .background(
+                    Color.LightGray,
+                    RoundedCornerShape(50)
+                )
+        )
+        Text(
+            text=title,
+            style=MaterialTheme.typography.headlineSmall
+        )
+
+    }
+}
