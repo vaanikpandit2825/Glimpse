0bc754faeb11e20fec89e3129e6fe47c3c0f0ebc
vaanikpandit2825 <vaanikpandit@gmail.com>
2026-08-11 23:44:06 +0530

    feat: add profile navigation and Cloudinary setup

--- app/src/main/java/com/example/glimpse/firebase/FirebaseRepository.kt
+++ app/src/main/java/com/example/glimpse/firebase/FirebaseRepository.kt
@@ -1,22 +1,22 @@
 package com.example.glimpse.firebase
 
-import com.google.firebase.database.FirebaseDatabase
 import android.util.Log
-import com.google.firebase.FirebaseApp
 import com.example.glimpse.model.UserLocation
-import com.google.firebase.firestore.auth.User
-import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.FirebaseDatabase
 
 class FirebaseRepository {
+
     private val database = FirebaseDatabase.getInstance(
         "https://glimpse-e0aab-default-rtdb.asia-southeast1.firebasedatabase.app"
     )
-    private val usersRef = database.getReference("users")
+
+    private val locationsRef = database.getReference("locations")
 
     fun updateLocation(
         uid: String,
         latitude: Double,
-        longitude: Double
+        longitude: Double,
+        onSuccess:()->Unit={}
     ) {
         val location = hashMapOf(
             "latitude" to latitude,
@@ -27,29 +27,40 @@ class FirebaseRepository {
         Log.d("FIREBASE", "UID = $uid")
         Log.d("FIREBASE", "Database URL = ${database.reference}")
 
-        usersRef.child(uid)
+        locationsRef.child(uid)
             .setValue(location)
             .addOnSuccessListener {
-                Log.d("FIREBASE", "WRITE SUCCESS")
+                Log.d("FIREBASE", "LOCATION WRITE SUCCESS")
 
-                usersRef.child(uid)
+                locationsRef.child(uid)
                     .get()
                     .addOnSuccessListener { snapshot ->
-                        Log.d("FIREBASE", "READ BACK = ${snapshot.value}")
+                        Log.d(
+                            "FIREBASE",
+                            "LOCATION READ BACK = ${snapshot.value}"
+                        )
                     }
                     .addOnFailureListener { error ->
-                        Log.e("FIREBASE", "READ BACK FAILED", error)
+                        Log.e(
+                            "FIREBASE",
+                            "LOCATION READ BACK FAILED",
+                            error
+                        )
                     }
             }
             .addOnFailureListener { error ->
-                Log.e("FIREBASE", "WRITE FAILED", error)
+                Log.e(
+                    "FIREBASE",
+                    "LOCATION WRITE FAILED",
+                    error
+                )
             }
     }
 
     fun getUsersLocations(
         onResult: (List<UserLocation>) -> Unit
     ) {
-        usersRef.get()
+        locationsRef.get()
             .addOnSuccessListener { snapshot ->
 
                 val locations = snapshot.children.mapNotNull { child ->
@@ -65,7 +76,11 @@ class FirebaseRepository {
                 onResult(locations)
             }
             .addOnFailureListener { error ->
-                Log.e("FIREBASE", "Failed to fetch locations", error)
+                Log.e(
+                    "FIREBASE",
+                    "Failed to fetch locations",
+                    error
+                )
             }
     }
 }
