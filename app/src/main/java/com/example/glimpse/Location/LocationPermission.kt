da63c468d43c1af1a93715f64aca2448236e2d14
vaanikpandit2825 <vaanikpandit@gmail.com>
2026-08-11 23:44:39 +0530

    feat: add profile navigation and Cloudinary setup

--- app/src/main/java/com/example/glimpse/Location/LocationPermission.kt
+++ app/src/main/java/com/example/glimpse/Location/LocationPermission.kt
@@ -1,30 +1,38 @@
 package com.example.glimpse.Location
 
 import android.Manifest
-import android.annotation.SuppressLint
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import android.health.connect.datatypes.ExerciseRoute.Location
 
 @Composable
 fun RequestLocationPermission(
-    onPermissionGranted:()-> Unit
-){
-    var permissionGranted by remember { mutableStateOf(false) }
+    onPermissionGranted: () -> Unit
+) {
 
-    val launcher= rememberLauncherForActivityResult(
-        contract= ActivityResultContracts.RequestPermission()
-    ) { granted->
-        permissionGranted=granted
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
 
-        if(granted){
+        val fineGranted =
+            permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true
+
+        val coarseGranted =
+            permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true
+
+        if (fineGranted || coarseGranted) {
             onPermissionGranted()
         }
     }
+
     LaunchedEffect(Unit) {
         launcher.launch(
-            Manifest.permission.ACCESS_FINE_LOCATION
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
         )
     }
 }
