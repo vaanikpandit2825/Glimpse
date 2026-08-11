e624cec3d0eea543b1716d2cf8e8d2284e887e17
vaanikpandit2825 <vaanikpandit@gmail.com>
2026-08-11 23:43:18 +0530

    feat: add profile navigation and Cloudinary setup

--- app/src/main/java/com/example/glimpse/auth/AuthRepository.kt
+++ app/src/main/java/com/example/glimpse/auth/AuthRepository.kt
@@ -1,24 +1,56 @@
 package com.example.glimpse.auth
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
-class AuthRepository{
-    private val auth  =  FirebaseAuth.getInstance()
+class AuthRepository {
+
+    private val auth = FirebaseAuth.getInstance()
+
+    private val database = FirebaseDatabase.getInstance(
+        "https://glimpse-e0aab-default-rtdb.asia-southeast1.firebasedatabase.app"
+    )
+
+    private val profilesRef = database.getReference("profiles")
 
     fun signUp(
-        email:String,
-        password:String,
-        onSuccess:()->Unit,
-        onFailure:(Exception)->Unit
-    ){
-        auth.createUserWithEmailAndPassword(email,password)
-            .addOnSuccessListener{
-                onSuccess()
+        name: String,
+        email: String,
+        password: String,
+        onSuccess: () -> Unit,
+        onFailure: (Exception) -> Unit
+    ) {
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnSuccessListener { result ->
+
+                val uid = result.user?.uid
+
+                if (uid == null) {
+                    onFailure(Exception("User ID is null"))
+                    return@addOnSuccessListener
+                }
+
+                val profile = mapOf(
+                    "name" to name,
+                    "profilePhotoUrl" to ""
+                )
+
+                profilesRef
+                    .child(uid)
+                    .setValue(profile)
+                    .addOnSuccessListener {
+                        onSuccess()
+                    }
+                    .addOnFailureListener {
+                        onFailure(it)
+                    }
             }
             .addOnFailureListener {
                 onFailure(it)
             }
     }
+
     fun login(
         email: String,
         password: String,
