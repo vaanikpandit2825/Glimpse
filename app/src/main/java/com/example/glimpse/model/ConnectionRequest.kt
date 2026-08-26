c88e92c93ae4670a104b0654c9aa89a5b05ddefb
vaanikpandit2825 <vaanikpandit@gmail.com>
2026-08-26 11:26:54 +0530

    feat: implement incoming connection requests and sharing permissions foundation

---
+++ app/src/main/java/com/example/glimpse/model/ConnectionRequest.kt
@@ -0,0 +1,9 @@
+package com.example.glimpse.model
+
+data class ConnectionRequest(
+    val senderUid:String="",
+    val status: String="",
+    val createdAt:Long=0L,
+    val name:String="",
+    val profilePhotoUrl:String=""
+)
