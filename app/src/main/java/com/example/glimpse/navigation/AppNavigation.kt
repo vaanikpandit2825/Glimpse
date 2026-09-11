009efad36dd2b3f90178b98616bdc231d6122a70
vaanikpandit2825 <vaanikpandit@gmail.com>
2026-09-11 21:27:20 +0530

    feat: clean navigation stack after connection confirmation

--- app/src/main/java/com/example/glimpse/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/glimpse/navigation/AppNavigation.kt
@@ -35,7 +35,7 @@ fun AppNavigation(){
     }
     NavHost(
         navController = navController,
-        startDestination = "home"
+        startDestination = "login"
     )
     {
         composable("signup"){
